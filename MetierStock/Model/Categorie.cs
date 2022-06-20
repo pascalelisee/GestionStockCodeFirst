ff3fe5e3b7cd1dafd6c8adb3fb1ec657c04435fc
pascalelisee <80065473+pascalelisee@users.noreply.github.com>
2022-06-20 06:23:54 +0200

    Ajoutez des fichiers projet.

---
+++ MetierStock/Model/Categorie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+//valable pour les base de donnees
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MetierStock.Model
+{
+    public class Categorie
+    {
+
+    }
+}
